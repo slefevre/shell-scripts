d387d26ec71dbc5dd2b7061ed0c6ea2c1eb18ee6
Steve Lefevre <lefevre.10@osu.edu>
2022-09-24 13:09:51 -0400

    Updated find uniques script

--- find-uniques.bash
+++ find-uniques.bash
@@ -2,4 +2,4 @@
 
 # original script found at:
 #   https://superuser.com/a/426454/13743
-find DIR1 DIR2 -type f -exec sha1sum {} + | sort | uniq -u --check-chars 40 | cut -c 43-
+find $1 $2 -type f -exec sha1sum {} + | sort | uniq -u --check-chars 40 | cut -c 43-
