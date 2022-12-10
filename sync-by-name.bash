d459549c7c3d3d8fbf7869e0ce1c877c80e9f81e
Steve Lefevre <lefevre.10@osu.edu>
2022-12-09 22:47:25 -0500

    Banking changes to script

--- sync-by-name.bash
+++ sync-by-name.bash
@@ -11,6 +11,13 @@ find "$1" -type f -exec sh -c '
   for file do
     echo "$file"
     DIR="$(dirname "${file}")" ; FILE="$(basename "${file}")"
-    echo "[${DIR}] [${FILE}]"
+    echo "DIR: ${DIR}"
+    echo "FILE: ${FILE}"
+    echo "1: ${1}"
+    # RELPATH=$(echo $DIR | sed -e s/"^$1"//)
+    # echo "$RELPATH"
+    echo "-------"
   done
 ' exec-sh {} +
+
+
