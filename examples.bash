761d5519f5167741a9c4e9c8264226cd7c48cff1
Steve Lefevre <lefevre.10@osu.edu>
2023-04-23 20:14:52 -0400

    more examples

--- examples.bash
+++ examples.bash
@@ -8,9 +8,21 @@ exit 1;
 
 # store output in a variable
 VAR=$(command args)
-
 echo "$VAR";
 
+# check number of arguments
+if [ $# -lt 2 ]; then
+  # TODO: print usage
+  exit 1
+fi
 
-
+# test if value is dir or file
+if [[ -d $PASSED ]]; then
+   echo "$PASSED is a directory"
+elif [[ -f $PASSED ]]; then
+  echo "$PASSED is a file"
+else
+  echo "$PASSED is not valid"
+  exit 1
+fi
 
