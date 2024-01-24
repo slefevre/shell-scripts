59ce092b46a9181ba6c0a8808516bea311040a72
Steve Lefevre <steven.m.lefevre@gmail.com>
2024-01-24 14:05:31 -0500

    debugging initial argument

--- wsl-mount.bash
+++ wsl-mount.bash
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 # Exit on error
-set e
+set -e
 
 # Echo commands
-set x
+# set -x
 
 # test emtpy argument
-if [ -z "$1" ]; then
-	echo "Please specify a drive letter (e.g. f)."
+if [[ $# -eq 0 ]] ; then
+  echo "Please specify a drive letter (e.g. f)."
   exit 0
 fi
 
 DRIVELETTER=$(echo $1 | tr '[:lower:]' '[:upper:]')
 
-echo $DRIVELETTER
 sudo mkdir -p /mnt/$1
 # sudo mount -t drvfs "$DRIVELETTER:" /mnt/$1
