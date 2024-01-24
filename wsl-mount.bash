cb432607b5d7406659e5eea9eb16d0f7bc516aec
Steve Lefevre <steven.m.lefevre@gmail.com>
2024-01-24 13:49:23 -0500

    Added test for drive letter

--- wsl-mount.bash
+++ wsl-mount.bash
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# Exit on error
+set e
+
+# Echo commands
+set x
+
+# test emtpy argument
+if [ -z "$1" ]; then
+	echo "Please specify a drive letter (e.g. f)."
+  exit 0
+fi
+
 MOUNTPOINT=$1
 
 
-sudo mkdir /mnt/d
+sudo mkdir /mnt/$1
 sudo mount -t drvfs D: /mnt/d
