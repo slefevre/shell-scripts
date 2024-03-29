5ca70d405d40ec04d072bdddc1315003eeecf557
Steve Lefevre <steven.m.lefevre@gmail.com>
2024-03-29 16:07:04 -0400

    Added error checking in unmounting

--- wsl-unmount.bash
+++ wsl-unmount.bash
@@ -14,5 +14,13 @@ fi
 
 DRIVELETTER=$(echo $1 | tr '[:lower:]' '[:upper:]')
 
-sudo umount /mnt/$1
-sudo rmdir /mnt/$1
+if mountpoint -q /mnt/$1; then
+  if sudo umount -vvv /mnt/$1 && sudo rmdir /mnt/$1; then 
+    echo "$1 has been unmounted."
+  else
+    echo "Error unmounting $1"
+    exit -1
+  fi
+else
+  echo "$1 is not mounted."
+fi
