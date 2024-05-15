8872d55438736af4620c78c4ddb9ca6832f36c43
Steve Lefevre <lefevre.10@osu.edu>
2024-05-14 22:22:56 -0400

    troubleshooting

--- organize-screenshots.bash
+++ organize-screenshots.bash
@@ -1,9 +1,12 @@
 #!/bin/bash
+
+set -x
+
 for SCREENSHOT in ./*.jpg
 do
   SUBSTRING="${SCREENSHOT##*_}"
   DIR="$(basename $SUBSTRING .jpg)"
   echo "Moving $SCREENSHOT to  $DIR..."
-  mkdir -p $DIR
-  mv $SCREENSHOT $DIR
+  # mkdir -p $DIR
+  # mv $SCREENSHOT $DIR
 done
